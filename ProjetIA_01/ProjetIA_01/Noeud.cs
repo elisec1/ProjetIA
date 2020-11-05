aeff19b651957699c0d89b4542a5df2b7fadcd6b
elisec1 <57486955+elisec1@users.noreply.github.com>
2020-11-05 08:47:14 +0100

    bof

---
+++ ProjetIA_01/ProjetIA_01/Noeud.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetIA_01
+{
+    class Noeud : GenericNode
+    {
+
+        double X
+        {
+            get;set;
+        }
+        double Y
+        {
+            get;set;
+        }
+        public Noeud (double x, double y )
+        {
+            X = x;
+            Y = y;
+        }
+        public override bool IsEqual(GenericNode N2)
+        {
+            Noeud N = (Noeud)N2;
+            bool estEgal = false;
+            if (N.X == X && N.Y == Y)
+            {
+                estEgal = true;
+            }
+            return estEgal;
+        }
+
+        public override double CalculeHCost()
+        {
+            //distance jusqu'à la fin
+        }
+
+        public override double GetArcCost(GenericNode N2)
+        {
+            Noeud N = (Noeud)N2;
+            double coutArc = N2.GetGCost() - this.GetGCost();
+            return coutArc;
+        }
+        public override bool EndState()
+        {
+            throw new NotImplementedException(); 
+        }
+        public override List<GenericNode> GetListSucc()
+        {
+            //
+        }
+    }
+}
---
+++ ProjetIA_01/ProjetIA_01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetIA_01
+{
+    static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            Noeud pointDepart = new Noeud(0, 0);
+            Noeud pointArrivee = new Noeud(10, 18);
+            SearchTree Arbre = new SearchTree();
+        }
+    }
+}
